f8292c7b070c8cc5c20a12a138906f082397bcb6
Brice WEBER KLIPFEL <perso@briceweber.fr>
2021-10-25 01:52:45 +0200

    Add Vaisseau class

---
+++ TP1/Vaisseau.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1
+{
+    public class Vaisseau
+    {
+        private int _maxStructure, _maxShield;
+        private bool _isAlive;
+
+
+        public Vaisseau()
+        {
+        }
+
+        public int MaxStructure { get => _maxStructure; set => _maxStructure = value; }
+        public int MaxShield { get => _maxShield; set => _maxShield = value; }
+        public bool IsAlive { get => _isAlive; set => _isAlive = value; }
+    }
+}
