3be9097166b0abf9cb89b43b1019f08c798e74e5
Brice WEBER KLIPFEL <perso@briceweber.fr>
2021-10-25 02:02:06 +0200

    Add Arme class

---
+++ TP1/Arme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1
+{
+    public class Arme
+    {
+        public string _name;
+        public int _damage, _criticalDamage;
+        public enum _type
+        {
+            Direct,
+            Explosif,
+            Guidé
+        }
+    }
+}
