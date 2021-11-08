aa7ea729a284b7b7b5d2cddf3f6669b1e4104515
Brice WEBER KLIPFEL <perso@briceweber.fr>
2021-11-08 19:05:24 +0100

    Add F-18 and Tardis

---
+++ TP2/IAptitude.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2
+{
+    public interface IAptitude
+    {
+        void Utilise(List<Vaisseau> VesselList);
+    }
+}
